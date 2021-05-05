62e4dd1afdd9b47736a46781d352aafc615bf08b
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-05-05 01:02:39 -0700

    final?

--- EnigmaMachine.java
+++ EnigmaMachine.java
@@ -1,22 +1,33 @@
 public class EnigmaMachine{
    //define board and rotors
-   private int[] plugboard;
+   private Rotor plugboard;
    private int[] reflector;
    private Rotor rotor1;
    private Rotor rotor2;
    private Rotor rotor3;
    //Constructor, initialize board
    public EnigmaMachine(){
-      //plugboard definition
-      plugboard = new int[26];
+      /*Setup a new EnigmaMachine 
+         Input:
+             none
+         Output:
+             this: a new EnigmaMachine
+         Ex.
+         
+         EnigmaMachine encode = new EnigmaMachine()
+      */
+
+      //reflector def
       reflector = new int[26];
       //rotor setups
       int[] rot1 = new int[26];
       int[] rot2 = new int[26];
       int[] rot3 = new int[26];
+      int[] plugboard1 = new int[26];
       String rot1string = "DMTWSILRUYQNKFEJCAZBPGXOHV";
       String rot2string = "HQZGPJTMOBLNCIFDYAWVEUSRKX";
       String rot3string = "UQNTLSZFMREHDPXKIBVYGJCWOA";
+      String plugboardstring = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       
       for (int i = 0; i < 26; i ++){
          rot1[i] = charNumber(rot1string.charAt(i));
@@ -27,15 +38,14 @@ public class EnigmaMachine{
       for (int i = 0; i < 26; i ++){
          rot3[i] = charNumber(rot3string.charAt(i));
       }
-      
+      for (int i = 0; i < 26; i ++){
+         plugboard1[i] = charNumber(plugboardstring.charAt(i));
+      }
       rotor1 = new Rotor(rot1, 0);
       rotor2 = new Rotor(rot2, 0);
       rotor3 = new Rotor(rot3, 0);
-      //plugboard setup
-      for (int x = 0; x < 26; x++){
-         this.plugboard[x] = x;
-         
-      }
+      plugboard = new Rotor(plugboard1, 0);
+      
       /*for (int x = 0; x < 2; x++){
          for (int y = 0; y < 26; y++){
             this.reflector[x][y] = (char)(y + 97);
@@ -72,6 +82,25 @@ public class EnigmaMachine{
    }
 
    public String enigmaEncryptor(String input){
+      /*changes the input String into an encrypted output String, simulating an Enigma machine
+
+         Input:
+             String input: input string that needs to be encrypted/decrypted
+             
+         Output: 
+             String finalString: output string that has been through the enigma machine
+         
+         Ex.
+         EnigmaMachine encode = new EnigmaMachine();
+         EnigmaMachine decode = new EnigmaMachine();
+         String testInput = “hellomynameisalex”;
+         String encrypted = encode.enigmaEncryptor(testInput);
+         StdOut.println(encrypted);
+         StdOut.println(decode.enigmaEncryptor(encrypted))
+         scsqdhkgurjdehvze
+         hellomynameisalex
+      */
+
       String finalstring = "";
       
       for (int i = 0; i < input.length(); i++){
@@ -89,8 +118,7 @@ public class EnigmaMachine{
          }
          char thisChar = input.charAt(i);
          //plugboard
-         //if ((int)thisChar )
-         int pluggedChar = this.plugboard[charNumber(thisChar)];
+         int pluggedChar = this.plugboard.inToOut(charNumber(thisChar));
          //rotor 1
          int rot1char = rotor1.inToOut(pluggedChar);
          
@@ -107,14 +135,31 @@ public class EnigmaMachine{
          int rot5char = rotor2.outToIn(rot4char);
          //rotor 3
          int rot6char = rotor1.outToIn(rot5char);
+         //final plugboard
          
-         finalstring += (char)(rot6char + 97);
+         int finalInt = plugboard.outToIn(rot6char);
+         
+         finalstring += (char)(finalInt + 97);
          rotor1.rotate();
       }
       return finalstring;
    }
    
    public static int charNumber(char a){
+      /*changes the input char into an integer based on ascii decimal tables and its position in the
+         alphabet
+         
+         Input:
+             char a: the char that needs to be changed
+             
+         Output: 
+             int newInt: the int version of char a
+         
+         Ex.
+         EnigmaMachine.charNumber(‘a’)
+         0
+      */
+
       if (a <= 'z' && a >= 'a'){
          return a - 97;
       }
--- Rotor.java
+++ Rotor.java
@@ -5,29 +5,14 @@ public class Rotor{
       /*Setup a new rotor using the int[] r
       and an offset of o.
       Input:
-         int[] r: the int array representing the rotor's output alphabet
-         int o: the offset of the rotor (should start at 0)
+          int[] r: the int array representing the rotor's output alphabet
+          int o: the offset of the rotor (should start at 0)
       Output:
-         this: a new empty Tic Tac Toe board
+          this: a new rotor
       Ex.
       int[] rot = new int[26];
       Rotor rotor1 = new Rotor(rot, 0);
       rotor1.print()
-      0
-      0
-      0
-      0
-      0
-      0
-      0
-      0
-      0
-      0
-      0
-      0
-      0
-      0
-      0
       0
       0
       0
@@ -36,13 +21,37 @@ public class Rotor{
       rotorArr = r;
       offset = o;
    }
-   //input to output (int input)
-   //output to input (int output)
-   //rotate
    public int inToOut(int input){
+      /*return the output of the rotor at index input
+         Input:
+             int input: the index of the output you want
+             
+         Output: 
+             int output: the current rotor’s output at position input, including offset
+         Ex.
+         int[] rot = new int[26];
+         rot[3] = 2;
+         Rotor rotor1 = new Rotor(rot, 0);
+         rotor1.inToOut(3)
+         2
+      */
       return rotorArr[(input + offset) % 26];
    }
    public int outToIn(int output){
+      /*return the input of the rotor when the output is the int argument
+         Input:
+             int output: the output you want to find the index for
+             
+         Output: 
+             int input: the current rotor’s input to get int output, including offset
+             
+         Ex.
+         int[] rot = new int[26];
+         rot[3] = 2;
+         Rotor rotor1 = new Rotor(rot, 0);
+         rotor1.outToIn(2)
+         3
+      */
       for (int i = 0; i < 26; i++){
          if (rotorArr[i] == output){
             return ( i - offset + 26 ) % 26;
@@ -51,15 +60,78 @@ public class Rotor{
       return 0;
    }
    public void rotate(){
+      /*change the offset of the rotor by 1
+         Input:
+             none
+             
+         Output: 
+             none
+         Side effects:
+              offset increases by 1
+             
+         Ex.
+         int[] rot = new int[26];
+         Rotor rotor1 = new Rotor(rot, 1);
+         rotor1.rotate();
+         rotor1.print()
+         Offset: 2
+         0 0 0 0 0 0 0 0 … (continued)
+      */
       this.offset++;
    }
    public int returnOffset(){
+      /*return the offset of the current rotor
+         Input:
+             none
+             
+         Output: 
+             this.offset: the current offset
+             
+         Ex.
+         int[] rot = new int[26];
+         rot[3] = 2;
+         Rotor rotor1 = new Rotor(rot, 25);
+         rotor1.returnOffset()
+         25
+      */
       return this.offset;
    }
    public void changeOffset(int newInt){
+      /*change the offset of the current rotor
+         Input:
+             newInt: the integer that the new offset should be
+             
+         Output: 
+             none
+         
+         Side Effects:
+             offset = newInt (the offset is now equal to the int argument given)
+         Ex.
+         int[] rot = new int[26];
+         Rotor rotor1 = new Rotor(rot, 25);
+         rotor1.changeOffset(4);
+         rotor1.print()
+         Offset: 4
+         0 0 0 0 0 0 0… (continued)
+      */
       this.offset = newInt;
    }
    public void print(){
+      /*prints the offset and then each individual state of the rotor’s alphabet
+         Input:
+             none
+             
+         Output: 
+             StdOut: prints the virtual rotor
+         
+         Ex.
+         int[] rot = new int[26];
+         Rotor rotor1 = new Rotor(rot, 25);
+         rotor1.changeOffset(4);
+         rotor1.print()
+         Offset: 4
+         0 0 0 0 0 0 0… (continued)
+      */
       StdOut.println("Offset: " + this.offset);
       for (int i = 0; i < this.rotorArr.length; i++){
          StdOut.print(this.rotorArr[i] + " ");
